7d47921b60df008287f1f7d4a81558fa5ff4fc3f
Demo User <debian@beaglebone.localdomain>
2024-02-26 09:21:24 +0000

    updating github 3/28/24

---
+++ GuitarHero/GuitarHero.py
@@ -0,0 +1,36 @@
+import "button code"
+import "pdf to music code"
+import "led array"
+import "potentiometer code" #for changing bpm and song selection
+import "code for LED array for guitar tabs"
+
+
+class GuitarHero():
+    
+    
+    def __init__():
+        
+    def _setup(self):
+        #setup instances of each code imported
+    def song_selection(self):
+    
+    def pdf_to_music(self):
+    
+    def start_song(self):
+        
+    def tempo(self):
+        
+    def led_metronome(self):
+    
+    def display_tabs(self):
+    
+    def pause_song(self):
+        
+    def continue_song(self):
+    
+    def reset_song(self):
+    
+    def end_song(self):
+    
+    
+        
